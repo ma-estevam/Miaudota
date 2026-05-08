b17eff8cd98f592836359a6791852fad8d312429
GIOVANNA DE LIMA RIOS <giovanna.lrios@CAS0474775ma-1.local>
2026-05-07 21:59:40 -0300

    Iniciando um projeto em Angular, organizando pastas, aplicando Material Design e criando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentsComponent } from './components/components.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PetCardComponent } from './components/pet-card/pet-card.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PagesComponent } from './pages/pages.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PetsComponent } from './pages/pets/pets.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { EditarComponent } from './pages/editar/editar.component';
+import { DetalhesComponent } from './pages/detalhes/detalhes.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    NavbarComponent,
+    PetCardComponent,
+    FooterComponent,
+    PagesComponent,
+    HomeComponent,
+    PetsComponent,
+    CadastroComponent,
+    EditarComponent,
+    DetalhesComponent,
+    SobreComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/pet-card/pet-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-card',
+  templateUrl: './pet-card.component.html',
+  styleUrls: ['./pet-card.component.css']
+})
+export class PetCardComponent {
+
+}
