51985f0399dab3d7b232e0d21e8bfb7b8fe2f821
MARIA CLARA DE JESUS ESTEVAM <maria.cjestevam@CAS0474843ma-1.local>
2026-05-14 21:32:27 -0300

    feat: adiciona responsividade e componentização inicial

--- src/app/components/pet-card/pet-card.component.ts
+++ src/app/components/pet-card/pet-card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pet-card',
+  standalone: false,
   templateUrl: './pet-card.component.html',
   styleUrls: ['./pet-card.component.css']
 })
 export class PetCardComponent {
+
+  @Input() nome!: string;
+  @Input() idade!: string;
+  @Input() imagem!: string;
+  @Input() tipo!: string;
 
 }
